efdb918b6727edc24de00a21fd7facbc7c9b28f2
Andrew Caires <andrewcaires@live.com>
2021-10-27 02:20:45 -0300

    update

--- src/UtilsJS.ts
+++ src/UtilsJS.ts
@@ -1,3 +1,5 @@
 export * from './array';
 export * from './EventEmitter';
+export * from './mask';
+export * from './promise';
 export * from './type';
--- src/array.ts
+++ src/array.ts
@@ -1,6 +1,6 @@
 import { isArray, isDef } from './type';
 
-export const toArray = (value: any) => {
+export const toArray = (value: any): Array<any> => {
 
   return isArray(value) ? value : isDef(value) ? [value] : [];
 }
