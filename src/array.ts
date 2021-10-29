9c2520d7dbc75d18dd4231ab2d6a87aa94cb6ae4
Andrew Caires <andrewcaires@live.com>
2021-10-29 12:49:02 -0300

    add chunkArray

--- src/array.ts
+++ src/array.ts
@@ -1,5 +1,17 @@
 import { isArray, isDef } from './type';
 
+export const chunkArray = <T>(array: Array<T>, length: number = 1): Array<Array<T>> => {
+
+  const result: Array<Array<T>> = [];
+
+  for (let i = 0; i < array.length; i = i + (length)) {
+
+    result.push(array.slice(i, (i + length)));
+  }
+
+  return result;
+}
+
 export const toArray = (value: any): Array<any> => {
 
   return isArray(value) ? value : isDef(value) ? [value] : [];
