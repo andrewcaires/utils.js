4ec2be8f3045049a5ebbf3a6d169580a28c740f6
Andrew Caires <andrewcaires@live.com>
2022-03-16 09:30:55 -0300

    add denied object

--- src/object.ts
+++ src/object.ts
@@ -4,3 +4,8 @@ export const allowedObject = (allowed: string[], raw: objectRaw): objectRaw => {
 
   return allowed.reduce((obj, key) => ({ ...obj, [key]: raw[key] }), {});
 }
+
+export const deniedObject = (denied: string[], raw: objectRaw): objectRaw => {
+
+  return allowedObject(Object.keys(raw).filter((value) => denied.indexOf(value) == -1), raw);
+}
